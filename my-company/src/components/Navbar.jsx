b0375c30918dd6335d8601da57d15d69112d7080
Nonkululeko Miya <miyanonkululeko09@gmail.com>
2024-08-18 21:49:56 +0200

    modified navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <ul style={{ color: 'purple', backgroundColor: 'yellow'}}>
+      <ul style={{ color: 'purple', backgroundColor: 'yellow', display: 'flex', justifyContent: 'center'}}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
