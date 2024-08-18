6a516d8eae99c7c81cca881d59bfb4256f99c14b
Nonkululeko Miya <miyanonkululeko09@gmail.com>
2024-08-18 23:09:01 +0200

    modified app.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,13 +3,17 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import ProfilePage from './components/ProfilePage';
+import UserContext from '/components/UserContext';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
@@ -41,8 +45,14 @@ function App() {
       name="Alice" 
       age="25" 
       bio="Loves hiking and photography" />
+      
+      <ProfilePage userData={userData} />;
+
+      <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
     </>
-  )
+  );
 }
 
 export default App;
