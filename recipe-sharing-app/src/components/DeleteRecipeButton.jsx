d2e3457261c37adab9fba2889217f3007b4b8d8a
Nonkululeko Miya <miyanonkululeko09@gmail.com>
2024-08-25 19:05:33 +0200

    modify deletereipebutton

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
 import useRecipeStore from '../store';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
-  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const navigate = useNavigate();  
+    const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/recipes');
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
