37fcc3a14cde82ed0d66c62e925ca949b53d8a8f
Nonkululeko Miya <miyanonkululeko09@gmail.com>
2024-09-08 23:06:19 +0200

    added directory

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostsComponent from './PostsComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
